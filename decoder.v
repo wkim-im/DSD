7083a071077382787dc2e3229c3280272505a035
wkim-im <skdmlwka@gmail.com>
2025-06-08 22:12:22 +0900

    decoder 수정

--- decoder.v
+++ decoder.v
@@ -3,7 +3,10 @@ module decoder (
     input wire [1:0] IN,
     output wire [3:0] OUT
 );
-
-    assign OUT = EN?(4'b0001 << IN) : 4'b0000;
+    //data flow modeling , not use shifting <- 하드웨어 만들어 질때 오해의 소지가 있음
+    assign OUT[0] = EN? (~IN[1]&~IN[0]) : 1'b0; // assign 구문으로 할당
+    assign OUT[1] = EN? (~IN[1]&IN[0]) : 1'b0;
+    assign OUT[2] = EN? (IN[1]&~IN[0]) : 1'b0;
+    assign OUT[3] = EN? (IN[1]&IN[0]) : 1'b0;
 
 endmodule
